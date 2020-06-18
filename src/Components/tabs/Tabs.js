385e6d98e1efd893afd2e0158b72e2f60fee2854
Abhay Jain <abhayjain031198@gmail.com>
2020-06-18 22:06:09 +0530

    Tab using proper Js

--- src/Components/tabs/Tabs.js
+++ src/Components/tabs/Tabs.js
@@ -1,55 +1,121 @@
 import React, { useState } from "react";
 import "./Tabs.css";
 
-export default function Tabs() {
-  const [active, setActive] = useState(false);
-  const [tab, setTab] = useState(false);
+export default function Tabs(props) {
+  //   const [active, setActive] = useState(false);
+  //   const [tab, setTab] = useState(props.hide);
+  //   //   Product: true,
+  //   //   Design: true,
+  //   //   Engineering: true,
+  //   //   Marketing: true,
+  //   //   CustomerExperience: true,
+  //   //   HR: true,
+  //   //   Business: true,
+  //   //   Operations: true,
+  //   //   Finance: true,
+  //   // });
 
-  function openCity(event) {
-    var pro = String(event.target.getAttribute("value"));
-    alert(pro);
-    setActive(true);
-    setTab(true);
+  //   function openCity(event) {
+  //     var pro = String(event.target.getAttribute("value"));
+  //     alert(pro);
+  //     // setActive(true);
+  //     // setTab(true);
+  //   }
+
+  //   const aware = [
+  //     "Product",
+  //     "Design",
+  //     "Engineering",
+  //     "Marketing",
+  //     "Customer Experience",
+  //     "HR",
+  //     "Business",
+  //     "Operations",
+  //     "Finance",
+  //   ];
+  //   const Taware = aware.map((items) => {
+  //     return (
+  //       <div>
+  //         <a
+  //           className="anctag"
+  //           href="javascript:void(0)"
+  //           onClick={() => setTab(true)}
+  //         >
+  //           <div
+  //             value={items}
+  //             onClick={(event) => openCity(event)}
+  //             // className="paddingText tablink borderBottom"
+  //             className={
+  //               tab == true
+  //                 ? "borderBottom1 paddingText tablink"
+  //                 : "borderBottom paddingText tablink"
+  //             }
+  //           >
+  //             {items}
+  //           </div>
+  //         </a>
+  //       </div>
+  //     );
+  //   });
+
+  //   return (
+  //     <>
+  //       <h1>Tabs</h1>
+
+  //       <div className="singleLine">{Taware}</div>
+  //       {tab == true ? <h1>Abhay</h1> : ""}
+  //     </>
+  //   );
+  function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
   }
 
-  const aware = [
-    "Product",
-    "Design",
-    "Engineering",
-    "Marketing",
-    "Customer Experience",
-    "HR",
-    "Business",
-    "Operations",
-    "Finance",
-  ];
-  const Taware = aware.map((items) => {
-    return (
-      <div>
-        <a className="anctag" href="javascript:void(0)">
-          <div
-            value={items}
-            onClick={(event) => openCity(event)}
-            // className="paddingText tablink borderBottom"
-            className={
-              tab == true
-                ? "borderBottom1 paddingText tablink"
-                : "borderBottom paddingText tablink"
-            }
-          >
-            {items}
-          </div>
-        </a>
-      </div>
-    );
-  });
+  window.onload = function (e) {
+    document.getElementById("defaultOpen").click();
+  };
 
   return (
     <>
-      <h1>Tabs</h1>
+      <div class="tab">
+        <div
+          class="tablinks"
+          onClick={(event) => openCity(event, "London")}
+          id="defaultOpen"
+        >
+          London
+        </div>
+        <div class="tablinks" onClick={(event) => openCity(event, "Paris")}>
+          Paris
+        </div>
+        <div class="tablinks" onClick={(event) => openCity(event, "Tokyo")}>
+          Tokyo
+        </div>
+      </div>
 
-      <div className="singleLine">{Taware}</div>
-      {active == true ? <h1>Abhay</h1> : ""}
+      <div id="London" class="tabcontent">
+        <h3>London</h3>
+        <p>London is the capital city of England.</p>
+      </div>
+
+      <div id="Paris" class="tabcontent">
+        <h3>Paris</h3>
+        <p>Paris is the capital of France.</p>
+      </div>
+
+      <div id="Tokyo" class="tabcontent">
+        <h3>Tokyo</h3>
+        <p>Tokyo is the capital of Japan.</p>
+      </div>
     </>
   );
 }
