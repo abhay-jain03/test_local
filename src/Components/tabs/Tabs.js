38d26372cc96758371c1c51f59f3a63fc1e988eb
Abhay Jain <abhayjain031198@gmail.com>
2020-07-05 22:35:54 +0530

    Tabs Component With title

--- src/Components/tabs/Tabs.js
+++ src/Components/tabs/Tabs.js
@@ -16,6 +16,20 @@ export default function Tabs(props) {
     var title = "Product";
   } else if (tab == "Design") {
     var title = "Design";
+  } else if (tab == "Engineering") {
+    var title = "Engineering";
+  } else if (tab == "Marketing") {
+    var title = "Marketing";
+  } else if (tab == "Customer Experience") {
+    var title = "Customer Experience";
+  } else if (tab == "HR") {
+    var title = "HR";
+  } else if (tab == "Business") {
+    var title = "Business";
+  } else if (tab == "Operations") {
+    var title = "Operations";
+  } else if (tab == "Finance") {
+    var title = "Finance";
   }
 
   const aware = [
@@ -52,8 +66,15 @@ export default function Tabs(props) {
       <h1>Tabs</h1>
 
       <div className="singleLine">{Taware}</div>
-      {tab == "Product" ? <h1>Abhay</h1> : ""}
-      {tab == "Design" ? <h1>Jain</h1> : ""}
+      {tab == "Product" ? <h1>{title}</h1> : ""}
+      {tab == "Design" ? <h1>{title}</h1> : ""}
+      {tab == "Engineering" ? <h1>{title}</h1> : ""}
+      {tab == "Marketing" ? <h1>{title}</h1> : ""}
+      {tab == "Customer Experience" ? <h1>{title}</h1> : ""}
+      {tab == "HR" ? <h1>{title}</h1> : ""}
+      {tab == "Business" ? <h1>{title}</h1> : ""}
+      {tab == "Operations" ? <h1>{title}</h1> : ""}
+      {tab == "Finance" ? <h1>{title}</h1> : ""}
     </>
   );
 }
