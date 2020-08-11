d85d8039600abfbbad1e2e0ee324374617181b9a
abhay-jain03 <abhayjain031198@gmail.com>
2020-08-11 23:32:14 +0530

    Working Tab view

--- src/Components/tabs/Tabs.js
+++ src/Components/tabs/Tabs.js
@@ -2,56 +2,46 @@ import React, { useState } from "react";
 import "./Tabs.css";
 
 export default function Tabs(props) {
-  // const [active, setActive] = useState(false);
   const [tab, setTab] = useState("Product");
-
-  function openCity(event) {
-    var pro = String(event.target.getAttribute("value"));
-    alert(pro);
-    // setActive(true);
-    setTab(pro);
-  }
-
-  if (tab == "Product") {
-    var title = "Product";
-  } else if (tab == "Design") {
-    var title = "Design";
-  }
+  const [active, setActive] = useState(false);
 
   const aware = [
-    "Product",
-    "Design",
-    "Engineering",
-    "Marketing",
-    "Customer Experience",
-    "HR",
-    "Business",
-    "Operations",
-    "Finance",
+    { name: "Product" },
+    { name: "Design" },
+    { name: "Engineering" },
+    { name: "Marketing" },
+    { name: "Customer Experience" },
+    { name: "HR" },
+    { name: "Business" },
+    { name: "Operations" },
+    { name: "Finance" },
   ];
-  const Taware = aware.map((items) => {
-    return (
-      <div>
-        <a className="anctag" href="javascript:void(0)">
-          <div
-            value={items}
-            onClick={(event) => openCity(event)}
-            className="borderBottom paddingText tablink"
-          >
-            {items}
-          </div>
-        </a>
-      </div>
-    );
-  });
 
   return (
     <>
       <h1>Tabs</h1>
 
-      <div className="singleLine">{Taware}</div>
-      {tab == "Product" ? <h1>Abhay</h1> : ""}
-      {tab == "Design" ? <h1>Jain</h1> : ""}
+      <div className="singleLine">
+        {aware.map((item, index) => {
+          return (
+            <>
+              <div
+                onClick={() => (setTab(item.name), setActive(index))}
+                className={`borderBottom paddingText ${
+                  tab == item.name ? "borderBottom1" : null
+                }`}
+              >
+                {item.name}
+              </div>
+            </>
+          );
+        })}
+      </div>
+
+      {active == 0 ? <div>Hello</div> : null}
+      {active == 1 ? <div>Hi</div> : null}
+      {active == 2 ? <div>How</div> : null}
+      {active == 3 ? <div>Are</div> : null}
     </>
   );
 }
