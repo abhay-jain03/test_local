d117488eeaab32da373c1a6604b4c7824242b2c8
Abhay Jain <abhayjain031198@gmail.com>
2020-10-27 23:50:59 +0530

    First Page

--- src/Components/test1/Test1.js
+++ src/Components/test1/Test1.js
@@ -5,7 +5,16 @@ import "./Test1.css";
 const Test1 = (props) => {
   return (
       <>
-        <h1>Welcome To Our Website</h1>
+        <div className="headerComponent">
+          <div className="headerLogo">Welcome To Our Website</div>
+          <ul className="ul-header">
+            <li>Home</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div style={{ padding: '2rem' }}>
+          <div>Hi</div>
+        </div>
       </>
   )
 }
